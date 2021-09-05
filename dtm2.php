46505dd0c7b6bc0d5261329daa1d6ca5151ba9a8
AQDEV <uzwebdevolopersn2@gmail.com>
2021-09-05 03:12:58 -0400

    add example site

--- dtm2.php
+++ dtm2.php
@@ -10,6 +10,7 @@
  ****See the GNU Affero General Public License for more details.
  ****You should have received a copy of the GNU General Public License along with Mandat.
  ****If not, see <http://www.gnu.org/licenses/>.
+Website: https://api.qalandar.uz/dtm.php?id=[ID]
  */
 
 header("Content-type:application/json");
