a42eb9575904010229380c0b40841abbff2aa772
AQDEV <uzwebdevolopersn2@gmail.com>
2021-09-05 02:39:53 -0400

    add config file

---
+++ myconfig.php
@@ -0,0 +1,14 @@
+<?php
+
+unset($CFG);
+$CFG = new stdClass;
+$CFG->token1 = "Your-real-bot-token";
+$CFG->channel_url = "Your-channel-url";
+$CFG->dtmurl = "https://api.qalandar.uz/dtm.php?id=";
+$CFG->db = [
+    "host" => "your-db-host",
+    "name" => "your-db-name",
+    "user" => "your-db-username",
+    "pass" => "your-db-password",
+];
+$CFG->channel_id = "your-channel-id";
